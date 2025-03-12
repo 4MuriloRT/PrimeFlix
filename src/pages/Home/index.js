a3fe0146a9f4cbd2c86c61d069b232da12fc073c
Murilo Taborda <murilotaborda09@gmail.com>
2025-03-12 12:53:37 -0300

    Adiciona funcao carregar mais filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,49 +3,55 @@ import api from "../../services/api.js";
 import { Link } from "react-router-dom"; 
 import "./home.css";
 
-function Home(){
+function Home() {
     const [filmes, setFilmes] = useState([]);
     const [loading, setLoading] = useState(true);
-    
-    useEffect(()=>{
+    const [page, setPage] = useState(1);
 
-        async function loadFilmes(){
-            const response = await api.get("movie/now_playing",{
-                params:{
-                    api_key: "07ceb7f5b400a434ed626dca93ac4d72",
-                    language: "pt-BR",
-                    page: 1,
-                }
-            })
-
-            setFilmes(response.data.results.slice(0,10));
-            setLoading(false);
-        }
-
-        loadFilmes();
-
+    useEffect(() => {
+        loadFilmes(page);
     }, []);
 
-    if(loading){
-        return(
+    async function loadFilmes(pageNumber) {
+        const response = await api.get("movie/now_playing", {
+            params: {
+                api_key: "07ceb7f5b400a434ed626dca93ac4d72",
+                language: "pt-BR",
+                page: pageNumber,
+            }
+        });
+
+        setFilmes((prevFilmes) => [...prevFilmes, ...response.data.results.slice(0, 10)]);
+        setLoading(false);
+    }
+
+    function loadMoreMovies() {
+        const nextPage = page + 1;
+        setPage(nextPage);
+        loadFilmes(nextPage);
+    }
+
+    if (loading) {
+        return (
             <div className="loading">
                 <h2>Carregando filmes...</h2>
             </div>
-        )   
+        );
     }
 
-    return(
+    return (
         <div className="container">
             <div className="lista-filmes">
-                {filmes.map((filme)=>{
-                    return(
-                        <article key={filme.id} >
-                            <strong>{filme.title}</strong>
-                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
-                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
-                        </article>
-                    )
-                })}
+                {filmes.map((filme) => (
+                    <article key={filme.id}>
+                        <strong>{filme.title}</strong>
+                        <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                        <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                    </article>
+                ))}
+            </div>
+            <div className="load-more">
+                <button onClick={loadMoreMovies}>Carregar mais</button>
             </div>
         </div>
     );
